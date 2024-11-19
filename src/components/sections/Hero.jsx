43142df704225a80e100eedf34c5ecb1bd709b8d
Mahmoud Mohamed <mahmoudmohamedm82@gmail.com>
2024-11-19 14:05:16 +0200

    me3

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/me2.jpg";
+import HeroImg from "../../images/me3.jpg";
 import HeroBgAnimation from "../HeroBgAnimation";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
