2e3474c623fe595bc4bf7039dc7756b3f31bbad1
Mahmoud Mohamed <mahmoudmohamedm82@gmail.com>
2025-04-02 08:04:42 +0200

    daily-tm2

--- src/data/constants.js
+++ src/data/constants.js
@@ -233,22 +233,6 @@ export const skills = [
 
 export const experiences = [
   {
-    id: 40,
-    title: "DailyTM",
-    description: "Saas Website",
-    image: require('../images/daily-tm.png'),
-    tags: [
-      "Next",
-      "TypeScript",
-      "shadcn/ui",
-      "Next.js server/ui",
-      "Tailwindcss",
-      "Framer Motion",
-    ],
-    category: "saas",
-    webapp: "https://daily-tm.vercel.app",
-  },
-  {
     id: 0,
     img: mostaql,
     role: "Full Stack Developer",
@@ -297,6 +281,22 @@ export const education = [
 ];
 
 export const projects = [
+  {
+    id: 40,
+    title: "DailyTM",
+    description: "Saas Website",
+    image: require('../images/daily-tm.png'),
+    tags: [
+      "Next",
+      "TypeScript",
+      "shadcn/ui",
+      "Next.js server/ui",
+      "Tailwindcss",
+      "Framer Motion",
+    ],
+    category: "saas",
+    webapp: "https://daily-tm.vercel.app",
+  },
   {
     id: 0,
     title: "Suburbia",
